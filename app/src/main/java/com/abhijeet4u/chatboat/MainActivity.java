f85c0393cc26f7decc696e7f62127ee7eb27e54a
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2017-07-25 01:22:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/abhijeet4u/chatboat/MainActivity.java
@@ -0,0 +1,73 @@
+package com.abhijeet4u.chatboat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import java.security.PrivateKey;
+import static com.google.firebase.auth.FirebaseAuth.*;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int SIGN_IN_REQUEST_CODE =1 ;
+    private void displayChatMessages() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+            Toast.makeText(this,
+                    "Welcome " + getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            displayChatMessages();
+
+
+
+
+        }
+
+
+    }
+}
